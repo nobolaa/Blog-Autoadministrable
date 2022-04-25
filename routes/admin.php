29008f6cedda717aee062d745d6c15c199d84dcc
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-25 15:13:40 +0200

    Made changes on sidebar of admin page:     - Modified title of admin page     - Changed route when clicking on title of admin page     - Created Dashboard link on sidebar
    Created option to go to admin page on profile dropdown
    
    Started CRUD for categories:
        - Created controller for categories under admin folder with all
          CRUD functions declared.
        - Created all routes for administration of categories.
        - Created views for creation, edit, list and details of categories.
        - Filled view index of categories to show list of categories.
        - On AdminLT3:
            - changed account settings for administrador
            - created option categorias

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('', [HomeController::class, 'index'])->name('admin.home');
+
+Route::resource('categories', CategoryController::class)->names('admin.categories');
